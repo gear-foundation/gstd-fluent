1bdfc4cb42c72bdae94831f323fadf51ea46a44a
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-07-13 11:33:26 +0300

    Implement `for_reply()` and `for_reply_as()` for `SendBuilder`

--- src/generated/send.rs
+++ src/generated/send.rs
@@ -71,6 +71,23 @@ impl<Program, Payload: PayloadWithGasReservationMarker, Value, Delay, GasLimit: 
     }
 }
 
+impl<Program, Payload, Value, GasLimit, ReservationId> SendBuilder<(Program, Payload, Value, (), GasLimit, ReservationId)> {
+    pub fn for_reply(self) -> SendBuilderForReply<(Program, Payload, Value, GasLimit, ReservationId, (), ())> {
+        let (program, payload, value, _, gas_limit, reservation_id) = self.fields;
+        SendBuilderForReply {
+            fields: (program, payload, value, gas_limit, reservation_id, (), ()),
+        }
+    }
+
+    #[allow(clippy::type_complexity)]
+    pub fn for_reply_as<Decodable: Decode>(self) -> SendBuilderForReply<(Program, Payload, Value, GasLimit, ReservationId, (), DecodableW<Decodable>)> {
+        let (program, payload, value, _, gas_limit, reservation_id) = self.fields;
+        SendBuilderForReply {
+            fields: (program, payload, value, gas_limit, reservation_id, (), DecodableW(PhantomData)),
+        }
+    }
+}
+
 impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>> SendBuilder<(ProgramW, PayloadBytesW<Buffer>, Value, (), (), ())> {
     pub fn execute(self) -> Result<MessageId> {
         let (ProgramW(program), PayloadBytesW(payload), value, _, _, _) = self.fields;
@@ -180,5 +197,135 @@ impl<Range: RangeBounds<usize>, Value: Into<ValueW>> SendBuilder<(ProgramW, Payl
     pub fn execute(self) -> Result<MessageId> {
         let (ProgramW(program), PayloadInputW(payload), value, DelayW(delay), GasLimitW(gas_limit), _) = self.fields;
         send_input_with_gas_delayed(program, gas_limit, value.into().0, payload, delay)
+    }
+}
+
+// ---------------------------------------------------------------------------------------------- //
+// bindings for `send*`: for_reply(), for_reply_as::<Decodable>()
+// SendBuilderForReply<(Program, Payload, Value, GasLimit, ReservationId, ReplyDeposit, Decodable)>
+// ---------------------------------------------------------------------------------------------- //
+
+pub struct SendBuilderForReply<Fields = ((), (), (), (), (), ())> {
+    fields: Fields,
+}
+
+impl<Program, Payload, Value, GasLimit, ReservationId, Decodable> SendBuilderForReply<(Program, Payload, Value, GasLimit, ReservationId, (), Decodable)> {
+    pub fn with_reply_deposit(self, reply_deposit: u64) -> SendBuilderForReply<(Program, Payload, Value, GasLimit, ReservationId, ReplyDepositW, Decodable)> {
+        let (program, payload, value, gas_limit, reservation_id, _, decodable) = self.fields;
+        SendBuilderForReply {
+            fields: (program, payload, value, gas_limit, reservation_id, ReplyDepositW(reply_deposit), decodable),
+        }
+    }
+}
+
+impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadBytesW<Buffer>, Value, (), (), ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadBytesW(payload), value, _, _, reply_deposit, _) = self.fields;
+        send_bytes_for_reply(program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadBytesW<Buffer>, Value, (), ReservationIdW, ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadBytesW(payload), value, _, ReservationIdW(reservation_id), reply_deposit, _) = self.fields;
+        send_bytes_from_reservation_for_reply(reservation_id, program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadBytesW<Buffer>, Value, GasLimitW, (), ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadBytesW(payload), value, GasLimitW(gas_limit), _, reply_deposit, _) = self.fields;
+        send_bytes_with_gas_for_reply(program, payload, gas_limit, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Encodable: Encode, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadEncodableW<Encodable>, Value, (), (), ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadEncodableW(payload), value, _, _, reply_deposit, _) = self.fields;
+        send_for_reply(program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Encodable: Encode, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadEncodableW<Encodable>, Value, (), ReservationIdW, ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadEncodableW(payload), value, _, ReservationIdW(reservation_id), reply_deposit, _) = self.fields;
+        send_from_reservation_for_reply(reservation_id, program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Encodable: Encode, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadEncodableW<Encodable>, Value, GasLimitW, (), ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadEncodableW(payload), value, GasLimitW(gas_limit), _, reply_deposit, _) = self.fields;
+        send_with_gas_for_reply(program, payload, gas_limit, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Range: RangeBounds<usize>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadInputW<Range>, Value, (), (), ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadInputW(payload), value, _, _, reply_deposit, _) = self.fields;
+        send_input_for_reply(program, value.into().0, payload, reply_deposit.into().0)
+    }
+}
+
+impl<Range: RangeBounds<usize>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>> SendBuilderForReply<(ProgramW, PayloadInputW<Range>, Value, GasLimitW, (), ReplyDeposit, ())> {
+    pub fn execute(self) -> Result<MessageFuture> {
+        let (ProgramW(program), PayloadInputW(payload), value, GasLimitW(gas_limit), _, reply_deposit, _) = self.fields;
+        send_input_with_gas_for_reply(program, gas_limit, value.into().0, payload, reply_deposit.into().0)
+    }
+}
+
+impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadBytesW<Buffer>, Value, (), (), ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadBytesW(payload), value, _, _, reply_deposit, _) = self.fields;
+        send_bytes_for_reply_as(program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadBytesW<Buffer>, Value, (), ReservationIdW, ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadBytesW(payload), value, _, ReservationIdW(reservation_id), reply_deposit, _) = self.fields;
+        send_bytes_from_reservation_for_reply_as(reservation_id, program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Buffer: AsRef<[u8]>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadBytesW<Buffer>, Value, GasLimitW, (), ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadBytesW(payload), value, GasLimitW(gas_limit), _, reply_deposit, _) = self.fields;
+        send_bytes_with_gas_for_reply_as(program, payload, gas_limit, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Encodable: Encode, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadEncodableW<Encodable>, Value, (), (), ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadEncodableW(payload), value, _, _, reply_deposit, _) = self.fields;
+        send_for_reply_as(program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Encodable: Encode, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadEncodableW<Encodable>, Value, (), ReservationIdW, ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadEncodableW(payload), value, _, ReservationIdW(reservation_id), reply_deposit, _) = self.fields;
+        send_from_reservation_for_reply_as(reservation_id, program, payload, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Encodable: Encode, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadEncodableW<Encodable>, Value, GasLimitW, (), ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadEncodableW(payload), value, GasLimitW(gas_limit), _, reply_deposit, _) = self.fields;
+        send_with_gas_for_reply_as(program, payload, gas_limit, value.into().0, reply_deposit.into().0)
+    }
+}
+
+impl<Range: RangeBounds<usize>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadInputW<Range>, Value, (), (), ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadInputW(payload), value, _, _, reply_deposit, _) = self.fields;
+        send_input_for_reply_as(program, value.into().0, payload, reply_deposit.into().0)
+    }
+}
+
+impl<Range: RangeBounds<usize>, Value: Into<ValueW>, ReplyDeposit: Into<ReplyDepositW>, Decodable: Decode> SendBuilderForReply<(ProgramW, PayloadInputW<Range>, Value, GasLimitW, (), ReplyDeposit, DecodableW<Decodable>)> {
+    pub fn execute(self) -> Result<CodecMessageFuture<Decodable>> {
+        let (ProgramW(program), PayloadInputW(payload), value, GasLimitW(gas_limit), _, reply_deposit, _) = self.fields;
+        send_input_with_gas_for_reply_as(program, gas_limit, value.into().0, payload, reply_deposit.into().0)
     }
 }
